147fed79710132c570bf3c7de7f452a36440ee51
condret <condr3t@protonmail.com>
2018-09-30 23:04:06 +0000

    split gbmmio into several structs as semantic reasonable groups

--- include/gb/gb.h
+++ include/gb/gb.h
@@ -6,20 +6,25 @@
 
 //pass this to an io-plugin, then map it to 0xff00
 //each register needs a set-fcn in the io-plugin which can trigger certain events
-typedef struct gameboy_memory_mapped_io_t {
-	ut8 p1;		//ff00
-	ut8 sb;		//ff01
-	ut8 sc;		//ff02
-	ut8 div;	//ff04
-	ut8 tima;	//ff05
-	ut8 tma;	//ff06
-	ut8 tac;	//ff07
-/*	--------	*/
-	ut8 _if;	//ff0f
-/*	--------	*/
-	ut8 nr[23];	//ff10 - ff26 //closed interval
-	ut8 wav[16];	//ff30 - ff3f
-/*	--------	*/
+typedef struct gameboy_memory_mapped_serial_transfere_t {
+	ut8 sb;		//0000 : ff01	
+	ut8 sc;		//0001 : ff02
+} GBMMST;
+
+typedef struct gameboy_memory_mapped_timers_t {
+	ut8 div;	//0000 : ff04
+	ut8 tima;	//0001 : ff05
+	ut8 tma;	//0002 : ff06
+	ut8 tac;	//0003 : ff07
+} GBMMTMR;
+
+typedef struct gameboy_memory_mapped_sound_t {
+	ut8 nr[23];	//0000 : ff10 - ff26 //closed interval
+// in plugin : ffffffffffffffff...
+	ut8 wav[16];	//0020 : ff30 - ff3f
+} GBMMSND;
+
+typedef struct gameboy_memory_mapped_screen_t {
 	ut8 lcdc;	//ff40
 	ut8 stat;	//ff41
 	ut8 scy;	//ff42
@@ -32,9 +37,22 @@ typedef struct gameboy_memory_mapped_io_t {
 	ut8 obp1;	//ff49
 	ut8 wy;		//ff4a
 	ut8 wx;		//ff4b
-} GBMMIO;
+} GBMMSCR;
 
 typedef struct gameboy_t {
-	GBMMIO mmio;
+	int hram_fd;		//this is r_io_fd_open (io, "malloc://0x7f", R_IO_RW | R_IO_EXEC, 0644);
+	ut32 hram_map_id;	//this is r_io_map_add (io, hram_fd, R_IO_RW | R_IO_EXEC, 0LL, 0xff80, 0x7f);
+	int vram_fd;		//this is r_io_fd_open (io, "malloc://0x2000", R_IO_RW | R_IO_EXEC, 0644);
+	ut32 vram_map_id;	//this is r_io_map_add (io, vram_fd, R_IO_RW | R_IO_EXEC, 0LL, 0x8000, 0x2000)->id;
+//for vblank and hblank we can just set the map->flags to 0 (use the API, don't d0 it by hand).
+//for dm access, via dma register, we can either temporary enable access via highlevel r_io_read_at
+//or we use the r_io_fd api, since the io-fd is still accessible, when the map is not
+/*	-----------	*/
+	ut8 joypad;		//ff00
+	GBMMST serial;
+	ut8 interrupt_flags;	//ff0f
+	GBMMTMR timers;
+	GBMMSCR screen;
+	ut8 interrupt_enable;	//ffff
 } Gameboy;
 
