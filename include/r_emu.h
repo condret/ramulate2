674e9baf7f89c51aafeea2e00147e640c445f5ed
condret <condr3t@protonmail.com>
2018-10-09 23:54:54 +0000

    try to provide wrappers for threading apis

--- include/r_emu.h
+++ include/r_emu.h
@@ -4,7 +4,6 @@
 #include <r_util.h>
 #include <r_io.h>
 #include <r_anal.h>
-#include <r_th.h>
 #include <r_emu_interactor.h>
 
 typedef struct r_emulator_t {
@@ -37,3 +36,27 @@ R_API REmu *r_emu_new();
 R_API bool r_emu_load(REmu *emu, char *arch);
 R_API void r_emu_unload(REmu *emu);
 R_API void r_emu_free (REmu *emu);
+
+#if __WINDOWS__
+#include <windows.h>
+#define	R_EMU_TH_TID	HANDLE
+/*	disgusting comma abuse hack below	*/
+/* this code make CriticalSection feel a bit like pthread_mutex, if nothing breaks */
+#define	r_emu_th_tid_create(x,y,z)	(((x)=CreateThread(NULL, 0, y, z, 0, NULL)), 0)
+#define	r_emu_th_tid_join(x)	WaitForSingleObject(x,INFINITE)
+#define	R_EMU_TH_LOCK	CRITICAL_SECTION
+#define	r_emu_th_lock_init(x)	(InitializeCriticalSection(x), 0)
+#define	r_emu_th_lock_fini(x)	(DeleteCriticalSection(x), 0)
+#define	r_emu_th_lock_lock(x)	(EnterCriticalSection(x), 0)
+#define	r_emu_th_lock_unlock(x)	(LeaveCriticalSection(x), 0)
+#else
+#include <pthread.h>
+#define	R_EMU_TH_TID	pthread_t
+#define	r_emu_th_tid_create(x,y,z)	pthread_create(x, NULL, y, z)
+#define	r_emu_th_tid_join(x)	pthread_join(x, NULL)
+#define	R_EMU_TH_LOCK	pthread_mutex_t
+#define	r_emu_th_lock_init(x)	pthread_mutex_init(x, PTHREAD_MUTEX_NORMAL)
+#define	r_emu_th_lock_fini(x)	pthread_mutex_destroy(x)
+#define	r_emu_th_lock_lock(x)	pthread_mutex_lock(x)
+#define	r_emu_th_lock_unlock(x)	pthread_mutex_unlock(x)
+#endif
